5b6e63a0a0266585d715b5510c273c0937e92aa1
Zunw <43964753+ZunwDev@users.noreply.github.com>
2022-04-25 00:01:13 +0200

    Added routing

---
+++ Frontend/src/router/router.js
@@ -0,0 +1,24 @@
+//import { Vue } from 'vue';
+import {
+    createRouter,
+    createWebHashHistory
+} from 'vue-router';
+import Authorization from '../components/Authorization.vue'
+import Main from '../components/Main.vue'
+
+let router = new createRouter({
+    history: createWebHashHistory(),
+    routes: [{
+            path: '/',
+            name: 'Authorization',
+            component: Authorization,
+        },
+        {
+            path: '/main',
+            name: 'Main',
+            component: Main,
+        }
+    ]
+})
+
+export default router;
